7fa7f0daa4e4d70023ee3b47f4f09cc44641fe2e
Mohammad Ali Nayeem <85398213+kazinayeem@users.noreply.github.com>
2022-11-19 12:18:48 +0600

    Update

--- components/Header.js
+++ components/Header.js
@@ -19,17 +19,7 @@ const Header = () => {
           <a className="navbar-brand" href="#">
             Navbar
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
@@ -71,82 +61,7 @@ const Header = () => {
       </nav>
 
 
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
 
-const Header = () => {
-  const [auth, setauth] = useState(null);
-
-  useEffect(() => {
-    if (localStorage.getItem("auth")) {
-      setauth(true);
-    } else {
-      setauth(false);
-    }
-  }, []);
-
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" href="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="/about">
-                  About
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link " href="/contact">
-                  Disabled
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link " href="/admin">
-                  Admin
-                </Link>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
-};
-
-export default Header;
 
     </div>
   );
